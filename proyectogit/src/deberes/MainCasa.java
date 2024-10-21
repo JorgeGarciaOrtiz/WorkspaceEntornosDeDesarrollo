60a74dc5f2bbc6d9d731b89232661e5b13616def
JorgeGarciaOrtiz <jgarort1903@g.educaand.es>
2024-10-21 14:43:03 +0200

    Version Casa

---
+++ proyectogit/src/deberes/MainCasa.java
@@ -0,0 +1,13 @@
+package deberes;
+
+public class MainCasa {
+
+	public static void main(String[] args) {
+
+		System.out.println("Hola mundo");
+		System.out.println("Hola mundo");
+		System.out.println("Hola mundo");
+
+	}
+
+}
