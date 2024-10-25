0568c4a7ecd0b57bfe4998640609fc0082c8aeff
JorgeGarciaOrtiz <jgarort1903@g.educaand.es>
2024-10-25 13:53:46 +0200

    Version 3

---
+++ proyectogit/src/proyectogit/Github.java
@@ -0,0 +1,12 @@
+package proyectogit;
+
+public class Github {
+
+	public static void main(String[] args) {
+
+		System.out.println("Hola mundo");
+		System.out.println("Version Facil");
+		System.out.println("Version 2 Facil");
+
+	}
+}
